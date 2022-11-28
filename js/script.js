cec17d0aea750268ea1f1362bf4943ccfae6bf72
VerSerhii <92516811+VerSerhii@users.noreply.github.com>
2022-11-28 19:28:19 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,73 @@
+"use strict"
+
+//Урок ПЕРШИЙ
+
+//Варіант 1
+// console.log('Вчимо JS')
+
+//Варіант 2
+// console.log('JS')
+// console.log('Вчимо')
+
+//Варіант 2
+// console.log('JS') console.log('Вчимо')
+
+//Варіант 3
+// console.log('JS);
+// console.log('Вчимо');
+
+
+//Урок ДРУГИЙ
+
+//Завдання №1
+// let userEyesColor = 'brown';
+// console.log(userEyesColor);
+
+
+//Завдання №2
+// let userName = 'Вася';
+// let user = userName;
+// console.log(userName);
+
+//Завдання №3
+
+//Варіант 1
+// let user = 'Вася' let age = 36
+
+//Варіант 2
+// let myage = 36;
+// console.log(myage);
+
+//Варіант 3
+// const BLOCK_SIZE = 14 + 50;
+// console.log(BLOCK_SIZE);
+
+//Варіант 4
+// const BLOCK_HEIGHT = 100;
+// console.log(BLOCK_HEIGHT);
+
+//Варіант 5
+// if (true) {
+//     var size = 15;
+//     console.log(size);
+// };
+
+//Урок ТРЕТІЙ
+//Варіант 1
+// let userAge = 36; 
+// let userInfo = 'Фрилансер ${userAge}';
+// console.log(userAge);
+// console.log(userInfo);
+
+//Варіант 2
+// let userHeight = 145 / 0;
+// console.log(userHeight);
+
+//Варіант 3
+// let userName;
+// console.log(userName);
+
+//Варіант 4
+let userSize = '45' / '8';
+console.log(userSize);
+console.log(typeof userSize);
